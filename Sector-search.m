4e22e8a260e1e1b88cbe59846fa6a00ffd988597
Arjunil <Arjunil@users.noreply.github.com>
2015-04-08 18:31:50 +0530

    Modified for dynamic vector change
    arguments modified: stop replaced by next to increase automation.

--- Sector-search.m
+++ Sector-search.m
@@ -1,28 +1,23 @@
-%Program to detect black line in white background
 
 % NOTE: X direction is the direction of columns and Y is the direction
 % along rows. Modify code accordingly
 
-img_path = ('L:\storage\Google Drive\Project\Images\straight.png');
-img1=imread(img_path);
-imshow(img1);
-hold on
-%user defined start and stop points(manually indicated) in [Y,X]
-start=[48 52];
-stop=[81 58];
+function point = sector_search(img1, radius, angle, start, next)
+
+
 %direction vector
-v = stop - start;
+v = next - start;
 %unit vector in that direction
 v_dir=v/norm(v);
 min_pos= [0 0];
-RADIUS=5;
-ANGLE=90;
+%RADIUS=5;
+%ANGLE=90;
 min_val= 255;
 perimeter_point_LI = min_pos;
 % count =0 ;
 % count2 =0 ;
 
-for theta = -ANGLE:ANGLE ;
+for theta = -angle:5:angle ;
 %     count = count +1; To count no. of iterations of loop
     rotation_matrix = [cosd(theta) -sind(theta); sind(theta) cosd(theta)];
     %roatating direction vector
@@ -30,7 +25,7 @@ for theta = -ANGLE:ANGLE ;
     %finding new point in image matrix 'Radius' distance ahead of current
     %point
     
-    perimeter_point = start + round(rotated_vdir .* RADIUS);
+    perimeter_point = next + round(rotated_vdir .* radius);
     %If same point detected again, continue to next iteration
     if (perimeter_point_LI == perimeter_point)
         continue
@@ -48,12 +43,8 @@ for theta = -ANGLE:ANGLE ;
         min_pos = perimeter_point;
     end
 end
-disp('The minimum position is')
-min_pos
-disp('The pixel value at this position')
-img1(min_pos(2),min_pos(1))
+plot(min_pos(1),min_pos(2), 'g*');
+point = min_pos;
 % count
 % count2
-    
-
-
+end    
